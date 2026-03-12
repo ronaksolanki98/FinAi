3b0aeaaa1085211e3380f5143a36f85002d3b5f7
ronak-cn-98 <ronaksolanki@Ronaks-MacBook-Air.local>
2026-03-12 18:45:59 +0530

    k8s

--- infra/terraform/provider.tf
+++ infra/terraform/provider.tf
@@ -3,14 +3,21 @@ terraform {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~>3.0"
+      
     }
+    
   }
 
   required_version = ">=1.5"
 }
 
 provider "azurerm" {
-  features {}
-}
+  features {
+    key_vault {
+      purge_soft_delete_on_destroy = false
+      recover_soft_deleted_key_vaults = false
+    }
+  }
+  }
 
 data "azurerm_client_config" "current" {}
