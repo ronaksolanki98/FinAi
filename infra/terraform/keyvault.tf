e5654f13a0584878b7dd0f2c4e6f822cabd4a5f2
ronak-cn-98 <ronaksolanki@Ronaks-MacBook-Air.local>
2026-03-15 20:46:42 +0530

    Commit

--- infra/terraform/keyvault.tf
+++ infra/terraform/keyvault.tf
@@ -1,5 +1,5 @@
 resource "azurerm_key_vault" "kv" {
-  name                = "finaikeyvault123"
+  name                = "keyvault123"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   tenant_id           = data.azurerm_client_config.current.tenant_id
